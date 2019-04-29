63501353922bdced339751a0c04d0f423cd65e75
shaolinmkz <nwabuzor.obiora@gmail.com>
2019-04-29 11:22:43 +0100

    feat(Modify):Edit and delete
    1. Implement the delete functionality accross the app
    2. Implement the update functionality across the app
    3. Write test
    
    [#165661403 Finishes]

--- src/__tests__/components/Admin.spec.jsx
+++ src/__tests__/components/Admin.spec.jsx
@@ -1,8 +1,7 @@
 import "babel-polyfill";
 import React from "react";
-import moxios from "moxios";
 import sinon from "sinon";
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
@@ -15,14 +14,14 @@ import HelperUtils from "../../utils/helperUtils";
 sinon.stub(window, "scroll");
 sinon.stub(window.location, "assign");
 
-const mockFunc = jest.fn();
+// const mockFunc = jest.fn();
 
-const e = {
-  preventDefault: mockFunc,
-  target: {
-    files: ["file1", "file2"]
-  }
-};
+// const e = {
+//   preventDefault: mockFunc,
+//   target: {
+//     files: ["file1", "file2"]
+//   }
+// };
 
 const mockUser = {
   firstname: "mockUser",
@@ -49,7 +48,7 @@ describe("Test the Admin component with mount", () => {
   it("It should render the Admin component and find an HTML section tag", () => {
     expect(AdminWrapper.find("section#admin").exists()).toBe(true);
   });
-  
+
   it("It should mock the map state to prop function", done => {
     const userReducer = {
       userData: {
